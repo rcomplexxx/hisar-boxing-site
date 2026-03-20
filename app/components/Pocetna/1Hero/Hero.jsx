b8001b70ddccb3a0e21684e97b9b575547abc772
lil_kaska <whatever>
2026-03-20 19:51:47 +0100

    :update

--- app/components/Pocetna/1Hero/Hero.jsx
+++ app/components/Pocetna/1Hero/Hero.jsx
@@ -4,7 +4,7 @@ import styles from './hero.module.css'
 
 export default function Hero() {
   return (
-    <section id="hero" className={`${styles.hero} min-h-screen relative`}>
+    <section id="hero" className={`${styles.hero}`}>
 
 
          <div className={styles.outerWrapper}>
@@ -14,7 +14,7 @@ export default function Hero() {
               GRUPNI I INDIVIDUALNI TRENINZI BOKSA U LESKOVCU
             </h1>
 
-            <p>
+            <p className={styles.heroPharagraph}>
               Naučite boks uz profesionalnog trenera sa iskustvom od 45 profesionalnih mečeva. Naučite pravilnu tehniku, taktiku i disciplinu koja stoji iza ovog sporta. Treninzi su prilagođeni vašim ciljevima – bilo da želite bolju formu, samoodbranu ili takmičarski nivo. Uđite u ring spremni i sigurni u svoje sposobnosti.
             </p>
 
