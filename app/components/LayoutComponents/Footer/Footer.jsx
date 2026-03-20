bc2332322a8e689e42144060d05179580c0a8a1a
lil_kaska <whatever>
2026-03-20 03:13:20 +0100

    :update

--- app/components/LayoutComponents/Footer/Footer.jsx
+++ app/components/LayoutComponents/Footer/Footer.jsx
@@ -1,30 +1,51 @@
-import styles from './footer.module.css';
-
-
-import React from 'react'
+import styles from "./footer.module.css";
+import React from "react";
 
 export default function Footer() {
   return (
-   <footer className={styles.footer}>
-  <div className={styles.contact}>
-    <p>📧 <a href="mailto:hisarboxing@gmail.com">hisarboxing@gmail.com</a></p>
-    <p>📞 <a href="tel:+381600000000">+381 60 000 0000</a></p>
-    <p>📍 Stara Pazova, Serbia</p>
-  </div>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        
+        {/* Brand */}
+        <div className={styles.col}>
+          <h3 className={styles.logo}>Hisar Boxing</h3>
+          <p className={styles.desc}>
+            Profesionalni boks klub u Staroj Pazovi. Trening za početnike i napredne.
+          </p>
+        </div>
 
-  <div className={styles.links}>
-    <a href="">Pocetna</a>
-    <a href="#our-classes">Nas tim</a>
-    <a href="#cta">Galerija</a>
-    <a href="#cta">O nama</a>
-    <a href="#cta">Kontaktirajte nas</a>
-  </div>
+        {/* Navigation */}
+        <div className={styles.col}>
+          <h4>Navigacija</h4>
+          <a href="/">Početna</a>
+          <a href="#our-classes">Naš tim</a>
+          <a href="#gallery">Galerija</a>
+          <a href="#about">O nama</a>
+          <a href="#contact">Kontakt</a>
+        </div>
 
-  <div className={styles.copy}>
-    <p>© {new Date().getFullYear()} Hisar Boxing Gym. Sva prava zadržana.</p>
-  </div>
-</footer>
-  )
+        {/* Contact */}
+        <div className={styles.col}>
+          <h4>Kontakt</h4>
+          <p>📧 <a href="mailto:hisarboxing@gmail.com">hisarboxing@gmail.com</a></p>
+          <p>📞 <a href="tel:+381600000000">+381 62 123456</a></p>
+          <p>📍 Devet jugovica 24, Leskovac</p>
+        </div>
+
+        {/* Extra (makes it feel legit) */}
+        <div className={styles.col}>
+          <h4>Radno vreme</h4>
+          <p>Pon - Pet: 08:00 - 22:00</p>
+          <p>Subota: 10:00 - 18:00</p>
+          <p>Nedelja: Zatvoreno</p>
+        </div>
+
+      </div>
+
+      {/* Bottom bar */}
+      <div className={styles.bottom}>
+        <p>© {new Date().getFullYear()} Hisar Boxing Gym. Sva prava zadržana.</p>
+      </div>
+    </footer>
+  );
 }
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 
 
 
-import NumberItem from "./components/Pocetna/2NumberCopyWriting/NumberItem";
-import Navbar from './components/LayoutComponents/Navbar/Navbar'
 import Hero from './components/Pocetna/1Hero/Hero'
 import FirstSection from './components/Pocetna/3FirstSection/FirstSection'
 import Faq from './components/Pocetna/4Faq/Faq'
-import Footer from './components/LayoutComponents/Footer/Footer'
 import NumberCopyWriting from './components/Pocetna/2NumberCopyWriting/NumberCopyWriting'
-import Image from "next/image";
+
 
 export default function Pocetna() {
  
