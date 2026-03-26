b059156ea064f52c59754814a4357dd85d4c7324
lil_kaska <whatever>
2026-03-26 17:58:30 +0100

    :update

--- app/components/Pocetna/2NumberCopyWriting/NumberCopyWriting.jsx
+++ app/components/Pocetna/2NumberCopyWriting/NumberCopyWriting.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import NumberItem from './NumberItem'
+import React from "react";
+import styles from "./numbercopywriting.module.css";
+import NumberItem from "./NumberItem";
 
 export default function NumberCopyWriting() {
   return (
-    <section className="bg-black text-white px-10 py-12 flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
-        
-          <div className="flex w-100 md:w-1/2 justify-around mb-6 md:mb-0">
-          <NumberItem number="15" text="Godina postojimo" />
-          <NumberItem number="9" text="Osvojenih zlatnih medalja" />
-          
-        </div>
-        
-        <div className="flex w-100 md:w-1/2 justify-around mb-6 md:mb-0 z-1">
-        <NumberItem number="3" text="Profesionalnih instruktora" />
-          <NumberItem number="6" text="Termina nedeljno" />
-          
-        </div>
+    <section className={styles.section}>
+      <div className={styles.column}>
+        <NumberItem number="15" text="Godina postojimo" />
+        <NumberItem number="9" text="Osvojenih zlatnih medalja" />
+      </div>
 
-      
-      </section>
-  )
+      <div className={styles.column}>
+        <NumberItem number="3" text="Profesionalnih instruktora" />
+        <NumberItem number="6" text="Termina nedeljno" />
+      </div>
+    </section>
+  );
 }
 
     
