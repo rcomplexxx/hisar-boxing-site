e5050d9e41deb08fa1bdfbe4a10f9e2b2f3faa5e
lil_kaska <whatever>
2026-04-06 09:03:38 +0200

    :update

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@
 import Hero from './components/Pocetna/1Hero/Hero'
 import FirstSection from './components/Pocetna/3FirstSection/FirstSection'
 import SecondSection from './components/Pocetna/3FirstSection/SecondSection'
-import Faq from './components/Pocetna/4Faq/Faq'
-import TrustSection from './components/Pocetna/TrustSection/TrustSection'
-import AtmosphereSlider from './AtmosphereSlider/AtmosphereSlider'
+import Faq from './components/Pocetna/5Faq/Faq'
+import TrustSection from './components/Pocetna/2TrustSection/TrustSection'
+import AtmosphereSlider from './components/Pocetna/4AtmosphereSlider/AtmosphereSlider'
 
 
 export default function Pocetna() {
