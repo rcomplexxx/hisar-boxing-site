c0576c0d39412a19b6cf17112d3afa6ee390b101
lil_kaska <whatever>
2026-04-06 08:09:50 +0200

    :update

---
+++ app/AtmosphereSlider/AtmosphereSlider.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import styles from "./atmosphereslider.module.css";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const images = [
+  "/images/gym_atmosphere_1.jpg",
+  "/images/gym_atmosphere_2.jpg",
+  "/images/gym_atmosphere_3.jpg",
+  "/images/gym_atmosphere_4.jpg",
+  "/images/gym_atmosphere_5.jpg",
+];
+
+export default function AtmosphereSlider() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Atmosfera na treningu</h2>
+
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={20}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{ delay: 5000 }}
+           navigation={{
+      prevEl: `.${styles.prev}`,
+      nextEl: `.${styles.next}`,
+    }}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {images.map((src, index) => (
+            <SwiperSlide key={index}>
+              <div className={styles.card}>
+                <img
+                  src={src}
+                  alt={`Training ${index + 1}`}
+                  className={styles.image}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import FirstSection from './components/Pocetna/3FirstSection/FirstSection'
 import SecondSection from './components/Pocetna/3FirstSection/SecondSection'
 import Faq from './components/Pocetna/4Faq/Faq'
 import TrustSection from './components/Pocetna/TrustSection/TrustSection'
+import AtmosphereSlider from './AtmosphereSlider/AtmosphereSlider'
 
 
 export default function Pocetna() {
@@ -21,6 +22,8 @@ export default function Pocetna() {
       <FirstSection/>
 
       <SecondSection/>
+
+      <AtmosphereSlider/>
 
 
    
